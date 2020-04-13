9b6bf8f1f075ccd9fa139ee5be7d1a5e00d11500
Jay A <justynadusei@gmail.com>
2020-04-13 11:07:23 +0000

    Adding comments

--- Swift100Milestone2/ViewController.swift
+++ Swift100Milestone2/ViewController.swift
@@ -10,26 +10,100 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var shoppingList: [String] = [String]()
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Adding nav item button (add)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addShoppingListItems))
+        let defaults = UserDefaults.standard
+        
+        if let savedItems = defaults.object(forKey: "shoppingList") as? Data {
+            
+            shoppingList = NSKeyedUnarchiver.unarchiveObject(with: savedItems) as! [String]
+            
+        }
+        
+        title = "Shopping List"
+        
+        // Adding add button to nav bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForItem))
+        
+        // Adding share button to nav bar
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         
     }
-
     
-    @objc func addShoppingListItems() {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        // Setting up alert to add shopping item
-        let ac = UIAlertController(title: "Enter your item", message: nil, preferredStyle: .alert)
-        // Addind a textfield to alert controller
+        // Adding rows = number of items in array
+        return shoppingList.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Filling tableview cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Editing cell in tableview
+        let ac = UIAlertController(title: "Edit Item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.textFields![0].text! = shoppingList[indexPath.row]
+        
+        ac.addAction(UIAlertAction(title: "Save", style: .default) { [unowned self] (action: UIAlertAction) in
+            self.shoppingList[indexPath.row] = ac.textFields![0].text!
+            self.tableView.reloadData()
+            self.saveItems()
+            
+        })
+        
+        ac.addAction(UIAlertAction(title: "Delete", style: .destructive) { [unowned self] (action: UIAlertAction) in
+            self.shoppingList.remove(at: indexPath.row)
+            self.tableView.reloadData()
+            self.saveItems()
+        })
+        
+        present(ac, animated: true)
+    }
+    
+   @objc func promptForItem() {
+        let ac = UIAlertController(title: "Enter Item", message: nil, preferredStyle: .alert)
         ac.addTextField()
         
+        ac.addAction(UIAlertAction(title: "Submit", style: .default) { [unowned self] (action: UIAlertAction) in
+            self.shoppingList.append(ac.textFields![0].text!)
+            self.tableView.reloadData()
+            self.saveItems()
+        })
+        
+        present(ac, animated: true)
     }
     
+   @objc func shareTapped() {
+        let list = shoppingList.joined(separator: "\n")
+        let initialText = "My shopping list: \n"
+        
+        let vc = UIActivityViewController(activityItems: [initialText, list], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.leftBarButtonItem
+        
+        present(vc, animated: true)
+    }
+    
+    func saveItems() {
+        let savedData = NSKeyedArchiver.archivedData(withRootObject: shoppingList)
+        let defaults = UserDefaults.standard
+        defaults.set(savedData, forKey: "items")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
-
