674a79655bdcad9691c25cc03d8dea5b508315e2
Jay A <justynadusei@gmail.com>
2020-04-13 10:11:32 +0000

    Created property to store shopping list items

--- Swift100Milestone2/ViewController.swift
+++ Swift100Milestone2/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var shoppingList: [String] = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @objc func addShoppingListItem
+    
 
 }
 
